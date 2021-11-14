1f8c3b7477f0abb6848f6fb2742bf5382bc6b35f
Jeff Smith <ascriptkiddy@aol.com>
2021-11-14 09:06:47 -0800

    adding new scripts

---
+++ ChristoMovieCopy.cmd
@@ -0,0 +1,10 @@
+robocopy E:/Videos/Action D:/Videos/Action /S
+robocopy E:/Videos/Adventure D:/Videos/Adventure /S
+robocopy E:/Videos/Comedy D:/Videos/Comedy /S
+robocopy E:/Videos/Drama D:/Videos/Drama /S
+robocopy E:/Videos/Fantasy D:/Videos/Fantasy /S
+robocopy E:/Videos/Horror D:/Videos/Horror /S
+robocopy "E:/Videos/New Releases" "D:/Videos/New Releases" /S
+robocopy E:/Videos/Scifi D:/Videos/Scifi /S
+robocopy E:/Videos/Suspense D:/Videos/Suspense /S
+robocopy E:/Videos/Thriller D:/Videos/Thriller /S
--- Copy Movies.cmd
+++ Copy Movies.cmd
@@ -1,5 +1,5 @@
 REM robocopy "\\FAKEMEDIA\Media\Video" "E:\Videos\Series" /S
 robocopy "C:\Torrents\To Copy\Series" "E:\Videos\Series" /S
-robocopy "C:\Torrents\To Copy\Series" "\\FAKEMEDIA\Media\Videos\Series" /S /MOV
+robocopy "C:\Torrents\To Copy\Series" "\\FAKEMEDIA\Video\Series" /S /MOV
 robocopy "C:\Torrents\To Copy" "E:\Videos" /S
-robocopy "C:\Torrents\To Copy" "\\FAKEMEDIA\Media\Videos" /S /MOV
+robocopy "C:\Torrents\To Copy" "\\FAKEMEDIA\Video" /S /MOV
